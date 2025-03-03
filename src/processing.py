cb237569d82e712697525356b5b000c776719f86
hhhh <mixertext@gmail.com>
2025-03-03 15:23:54 +0300

    Docstr and annot add

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,27 @@
-def filter_by_state(list_of_value):
+from datetime import datetime
+from typing import List, Dict
+
+
+def filter_by_state(list_of_value: List[Dict[str, str]]) -> List[Dict[str, str]]:
+    """
+    Фильтрует список словарей, оставляя только элементы с состоянием 'EXECUTED'.
+
+    :param list_of_value: Список словарей, содержащих ключ 'state'.
+    :return: Новый список, содержащий только элементы с 'state' == 'EXECUTED'.
+    """
     new_list = []
-    for i in list_of_value:
-        for k,v in i.items():
-            if v == 'EXECUTED':
-                new_list += i
-            else:
-                continue
+    for item in list_of_value:
+        if item.get("state") == "EXECUTED":
+            new_list.append(item)
     return new_list
 
 
-from datetime import datetime
-def sort_by_date(data, descending=True):
-    return sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=descending)
+def sort_by_date(data: List[Dict[str, str]], descending: bool = True) -> List[Dict[str, str]]:
+    """
+    Сортирует список словарей по дате.
+
+    :param data: Список словарей, содержащих ключ 'date'.
+    :param descending: Если True, сортировка по убыванию, иначе по возрастанию.
+    :return: Отсортированный список словарей.
+    """
+    return sorted(data, key=lambda x: datetime.fromisoformat(x["date"]), reverse=descending)
